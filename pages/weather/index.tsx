1bfcd2ccc26a51e0277ed768b11d28a2d7c32a97
Kyle Potts <kyle@kylepotts.com>
2022-11-24 15:54:15 -0500

    init commit

---
+++ pages/weather/index.tsx
@@ -0,0 +1,9 @@
+const Weather = () => {
+    return (
+        <div style={{height: 300, width: 400, display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Weather
